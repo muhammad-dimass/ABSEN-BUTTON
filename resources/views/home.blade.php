75b1bcdeae034aa709053720b2bd062060f7418b
muhammad-dimass <muhammad.diimass@gmail.com>
2023-02-27 17:45:38 +0700

    add folder template for looking in admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,23 @@
-@extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Adi-EXPO</title>
+        @include('backend.head')
+    </head>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<body class="g-sidenav-show  bg-gray-100">
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NKDMSK6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  @include('backend.leftbar')
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+  @include('backend.navbar')
+
+
+
+<!-- footer -->
+@include('backend.footer')
+<!-- footer -->
+
+</body>
+</html>
